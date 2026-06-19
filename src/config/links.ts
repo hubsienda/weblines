58189d722bda5ca570187c239dc262eff75c82e7
hubsienda <hub@siendaweblines.com>
2026-06-19 10:35:05 +0200

    Create links.ts

---
+++ src/config/links.ts
@@ -0,0 +1,13 @@
+export const GOOGLE_ENQUIRY_FORM_URL =
+  'https://docs.google.com/forms/d/e/1FAIpQLScuxiltnbo4XCA3T892yQaPd-aj-46lbxIGihgzWs8k9fg1gw/viewform';
+
+export const SIENDA_URL = 'https://sienda.co.uk';
+export const SIENDA_LEGAL_URL = 'https://sienda.co.uk/legal/';
+export const JAMBASTIC_URL = 'https://jambastic.com';
+export const QOOBIX_URL = 'https://qoobix.com';
+export const SIENDA_MEDIA_URL = 'https://siendamedia.com';
+
+export const TALLY_EMAIL_RISK_CHECKER_URL = 'https://tally.so/r/lb4dy5';
+export const TALLY_CONFIDENTIAL_COMMUNICATION_RISK_CHECKER_URL = 'https://tally.so/r/RGr8DK';
+export const TALLY_AI_THEATRE_DETECTOR_URL = 'https://tally.so/r/aQlYlb';
+export const TALLY_AUTOMATION_OR_AI_URL = 'https://tally.so/r/ZjQvZy';
